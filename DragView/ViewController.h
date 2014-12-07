efd81b718b6979fd98109cf0dd1c48c9d69bbd1a
fab3 <sunny@Sunnys-Mac-mini.local>
2014-12-07 17:45:59 +0530

    Initial Commit

---
+++ DragView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DragView
+//
+//  Created by fab3 on 07/12/14.
+//  Copyright (c) 2014 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DragView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DragView
+//
+//  Created by fab3 on 07/12/14.
+//  Copyright (c) 2014 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
